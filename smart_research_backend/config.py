ba4c59f3e29b1be7530e846b9ba9f3264456a1b7
MaryyamFakhar39@gmail.com <144408177+Maryyam18@users.noreply.github.com>
2025-12-08 16:02:33 +0500

    config

--- smart_research_backend/config.py
+++ smart_research_backend/config.py
@@ -13,5 +13,9 @@ class Settings:
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
+    
+SUPABASE_URL = "https://cujbuzouhjytygwghdjg.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1amJ1em91aGp5dHlnd2doZGpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2OTUxMTYsImV4cCI6MjA4MDI3MTExNn0.P0ow8BB44CNl_Y5Hkasw6W06Skv4D9J5jtLF4ndNb7k"
+
 
 settings = Settings()
