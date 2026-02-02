eb26477635f2192357016c00dc3995a98715364b
Hania Arshad <haniaarshad890@gmail.com>
2026-02-02 14:46:23 +0500

    files deleted

--- smart_research_backend/supabase_client.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client
-from config import settings
-
-if not settings.SUPABASE_URL or not settings.SUPABASE_KEY:
-    raise RuntimeError("SUPABASE_URL and SUPABASE_KEY must be set in environment")
-
-supabase = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
